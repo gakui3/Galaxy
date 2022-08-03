54273bc2d771d2c1a984c28e4adf0d58faf4075b
gKirii <gaku.kirii@gmail.com>
2022-08-03 12:45:44 +0900

    first commit

---
+++ docs/main.js
@@ -0,0 +1,111 @@
+import * as THREE from "three";
+import { GUI } from "three/examples/jsm/libs/dat.gui.module";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import testVert from "./shaders/test.vert";
+import testFrag from "./shaders/test.frag";
+import Planet from "./planet.js";
+import { earth, mercury, mars, venus, jupiter, saturn, uranus, neptune } from "./planetParams.js";
+
+let canvas, renderer, scene, camera, gui, clock;
+const planets = [];
+
+const param = {
+  value01: 1.0,
+  value02: true,
+  value03: 1.0,
+  value04: "hoge01",
+};
+
+function init () {
+  canvas = document.querySelector("#c");
+  renderer = new THREE.WebGLRenderer({ canvas });
+  window.renderer = renderer;
+  document.body.appendChild(renderer.domElement);
+  scene = new THREE.Scene();
+  clock = new THREE.Clock();
+  clock.start();
+}
+
+function addCamera () {
+  camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 1000);
+  camera.position.set(0, 0, -10);
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  window.camera = camera;
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 0, 0);
+  controls.update();
+}
+
+function addObject () {
+  planets.push(new Planet(mercury));
+  planets.push(new Planet(venus));
+  planets.push(new Planet(earth));
+  planets.push(new Planet(mars));
+  planets.push(new Planet(jupiter));
+  planets.push(new Planet(saturn));
+  planets.push(new Planet(uranus));
+  planets.push(new Planet(neptune));
+
+  for (let i = 0; i < planets.length; i++) {
+    scene.add(planets[i].mesh);
+  }
+}
+
+function addGUI () {
+  gui = new GUI();
+  const folder = gui.addFolder("folder");
+  gui.width = 300;
+
+  folder.add(param, "value01").onChange((value) => {
+    console.log(value);
+  });
+  folder.add(param, "value02").onChange((value) => {
+    console.log(value);
+  });
+  folder.add(param, "value03", 0, 2.0).onChange((value) => {
+    console.log(value);
+  });
+  folder.add(param, "value04", ["hoge01", "hoge02"]).onChange((value) => {
+    console.log(value);
+  });
+}
+
+function update () {
+  requestAnimationFrame(update);
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+
+  const dt = clock.getDelta() * 100000;
+  for (let i = 0; i < planets.length; i++) {
+    planets[i].update(dt, camera);
+  }
+
+  renderer.render(scene, camera);
+}
+
+function resizeRendererToDisplaySize (renderer) {
+  const canvas = renderer.domElement;
+  // const pixelRatio = window.devicePixelRatio;
+  const width = canvas.clientWidth;
+  // console.log(width);
+
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+(function () {
+  init();
+  addCamera();
+  addObject();
+  addGUI();
+  update();
+})();
---
+++ docs/planet.js
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+
+class Planet {
+  constructor (params) {
+    this.pos = new THREE.Vector3(params.x, params.y, params.z);
+    this.velocity = new THREE.Vector3(params.vx, params.vy, params.vz);
+
+    const geom = new THREE.SphereGeometry(1, 20, 20);
+    this.mesh = new THREE.Mesh(geom, new THREE.MeshBasicMaterial());
+    this.name = params.name;
+    this.mass = params.mass;
+  }
+
+  gm = 132712752000;
+
+  fv (p, n) {
+    const n3 = n ** 3;
+    return -1 * this.gm * p / n3;
+  }
+
+  fx (v) {
+    return v;
+  }
+
+  runge_kutta (dt, p, v, n) {
+    const kv1 = dt * this.fv(p, n);
+    const kx1 = dt * this.fx(v);
+
+    const kv2 = dt * this.fv(p + kx1 * 0.5, n);
+    const kx2 = dt * this.fx(v + kv1 * 0.5);
+
+    const kv3 = dt * this.fv(p + kx2 * 0.5, n);
+    const kx3 = dt * this.fx(v + kv2 * 0.5);
+
+    const kv4 = dt * this.fv(p + kx3, n);
+    const kx4 = dt * this.fx(v + kv3);
+
+    const nv = v + (1 / 6) * (kv1 + 2 * kv2 + 2 * kv3 + kv4);
+    const nx = p + (1 / 6) * (kx1 + 2 * kx2 + 2 * kx3 + kx4);
+    return [nx, nv];
+  }
+
+  update (dt, camera) {
+    let p = this.pos.clone();
+    const norm = p.length();
+    const xs = this.runge_kutta(dt, p.x, this.velocity.x, norm);
+    const ys = this.runge_kutta(dt, p.y, this.velocity.y, norm);
+    const zs = this.runge_kutta(dt, p.z, this.velocity.z, norm);
+
+    p = new THREE.Vector3(xs[0], ys[0], zs[0]);
+    const v = new THREE.Vector3(xs[1], ys[1], zs[1]);
+    this.pos = p;
+    this.velocity = v;
+
+    const _p = p.clone().multiplyScalar(0.00000005);
+    this.mesh.position.set(_p.x, _p.y, _p.z);
+
+    const canvas = window.renderer.domElement;
+    const worldPosition = this.mesh.getWorldPosition(new THREE.Vector3());
+    // const projection = worldPosition.project(camera);
+    const viewPosition = worldPosition.applyMatrix4(camera.matrixWorldInverse);
+    viewPosition.add(new THREE.Vector3(1.5, 0.5, 0));
+    const projection = viewPosition.applyMatrix4(camera.projectionMatrix);
+    const sx = (canvas.clientWidth / 2) * (projection.x + 1.0);
+    const sy = (canvas.clientHeight / 2) * (-projection.y + 1.0);
+
+    const parent = document.getElementById(this.name);
+    parent.innerHTML = `${this.name}:  ${Math.round(sx)}, ${Math.round(sy)}<br>mass: ${this.mass}`;
+    parent.style.transform = `translate(${sx}px, ${sy}px)`;
+  }
+}
+
+export default Planet;
